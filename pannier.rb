d6ed498f07134e1c0418b6c78d3f940dc508a434
Gray Prescott <gray@Grays-MacBook-Pro.local>
2023-11-19 17:39:56 -0800

    Split bike class into two classes, bike and pannier.

---
+++ pannier.rb
@@ -0,0 +1,27 @@
+class Pannier
+
+  MAX_CARGO_ITEMS = 10
+
+  attr_accessor :cargo_contents
+
+  def initialize
+    @cargo_contents = []
+  end
+
+  def add_cargo(item)
+    self.cargo_contents << item
+  end
+
+  def remove_cargo(item)
+    self.cargo_contents.remove(item)
+  end
+
+  def capacity
+    MAX_CARGO_ITEMS
+  end
+
+  def remaining_capacity
+    MAX_CARGO_ITEMS - self.cargo_contents.size
+  end
+
+end
